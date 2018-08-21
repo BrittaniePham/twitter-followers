893125562ca719ecd55ba6870b4168ac08cbe658
Brittanie Pham <brittaniepham@gmail.com>
2018-08-21 14:03:00 -0600

    previous

---
+++ client/src/components/Home.js
@@ -0,0 +1,113 @@
+import React, { Component, Fragment } from 'react';
+import { Header, Image, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import { StaggeredMotion, spring, presets } from 'react-motion';
+import Moved from './Moved'
+
+class Home extends Component {
+	state = { x: 250, y: 300, followers: [], moved: [] }
+
+	componentDidMount() {
+		window.addEventListener('mousemove', this.handleMouseMove);
+		window.addEventListener('dblclick', this.dropFollower);
+	}
+
+	getData = () => {
+		axios.get('/api/followers')
+		.then( res => this.setState({ followers: res.data }) )
+	}
+
+	dropFollower = () => {
+    const { moved, x, y } = this.state
+    const [drop, ...followers] = this.state.followers
+    this.setState({ 
+      followers,
+      moved: [...moved, { x, y, transitioned: false, ...drop }]
+    });
+	}
+
+	handleMouseMove = ({pageX: x, pageY: y}) => {
+		this.setState({x, y});
+	};
+
+	getStyles = (prevStyles) => {
+		// `prevStyles` is the interpolated value of the last tick
+		const endValue = prevStyles.map((_, i) => {
+			return i === 0
+				? this.state
+				: {
+					x: spring(prevStyles[i - 1].x, presets.gentle),
+					y: spring(prevStyles[i - 1].y, presets.gentle),
+				};
+		});
+		return endValue;
+	};
+
+  movedFollowers = () => {
+    const { moved } = this.state
+    const moveable = moved.filter( m => !m.transitioned )
+    return moveable.map( follower => {
+      return (
+        <Moved {...follower} addFollower={this.addFollower} key={follower.id} />
+      )
+    })
+  }
+
+  addFollower = (id) => {
+    const { moved, followers } = this.state
+    const follower = moved.find( f => f.id === id )
+    this.setState({
+      followers: [...followers, follower],
+      moved: moved.filter( m => m.id !== id )
+    })
+  }
+
+	render() {
+		return (
+			<Fragment>
+				<Button onClick={this.getData}>Click Me</Button>
+				{
+					this.state.followers.length > 0 &&
+          <Fragment>
+            { this.movedFollowers() }
+					  <StaggeredMotion
+					  	defaultStyles={this.state.followers.map(() => ({x: 0, y: 0}))}
+					  	styles={this.getStyles}>
+					  	{ avatars => 
+					  	<div style={{ width: '100%', height: '100%', position: 'absolute', background: '#EEE'}}>
+					  		{ avatars.map(({x,y}, i) => {
+					  			let follower = this.state.followers[i];
+					  			if (follower) {
+					  				return (
+					  					<div
+					  						key={i}
+					  						style={{
+					  							borderRadius: '99px',
+					  							backgroundColor: 'white',
+					  							width: '50px',
+					  							height: '50px',
+					  							border: '3px solid white',
+					  							position: 'absolute',
+					  							backgroundSize: '50px',
+					  							backgroundImage: `url(${this.state.followers[i].img})`,
+					  							transform: `translate3d(${x - 50}px, ${y - 50}px, 0)`,
+					  							zIndex: avatars.length - i,
+					  						}}
+					  					/>
+					  				)
+					  			} else {
+					  				return null
+					  			}
+					  			}
+					  			)}
+					  		</div>
+					  	}
+					  </StaggeredMotion>
+          </Fragment>
+				}
+			</Fragment>
+		);
+	};
+}
+
+export default Home;
